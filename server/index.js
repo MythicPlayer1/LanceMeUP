381e1c7baa206321cb9ddf0899e9cc7d6ea4c6bc
Ashish Rai <99983844+MythicPlayer1@users.noreply.github.com>
2023-02-28 22:05:14 +0545

    Add files via upload

--- server/index.js
+++ server/index.js
@@ -2,7 +2,11 @@ const express=require('express')
 const cors=require('cors');
 const registerSchema = require('./schemas/registerSchemas');
 const dbconnect=require('./dbconnection')
-const jwt= require('jsonwebtoken')
+const jwt= require('jsonwebtoken');
+// const uploadImage= require('./middleware/multer');
+const productSchema = require('./schemas/productSchema');
+const fs= require('fs');
+const multer=require('multer')
 
 
 const app= express();
@@ -12,6 +16,24 @@ const app= express();
 //middleware 
 app.use(cors())
 app.use(express.json());
+
+
+const uploadImage=  multer({
+    storage:multer.diskStorage({
+        destination:function(req, file, cb){
+            cb(null, 'uploadImageFiles');
+
+        },
+        filename:function(req, file, cb){
+            cb(null,file.fieldname+"-"+Date.now()+'.jpg'); 
+        }
+        
+        
+    })
+}).single("user_file");
+
+
+
 
 app.get('/', (req,resp)=>{
     resp.send("hhello from server side")
@@ -45,11 +67,29 @@ app.post('/api_login',async (req,resp)=>{
     }else{
         return(resp.json({status:'user not found', user:false}))
     }
-    // resp.send(user)
+     resp.send(user)
     
 })
 
 
+// api for product 
+
+app.post('/api_addproduct',uploadImage, async (req,resp)=>{
+    const result= await new productSchema({
+        name:req.body.name,
+        price:req.body.price,
+        des:req.body.des,nae,
+        img:{
+            data:fs.readFileSync("uploadImageFiles/" + req.file.filename),
+            contentType: "image/png/jpeg"
+        }
+    })
+    const data=result.save();
+
+     resp.send(data)
+
+
+})
 
 
 const start=async ()=>{
