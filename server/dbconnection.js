4be606dbb9b597fba585e315ba6a38fbc2f50d5d
Ashish Rai <99983844+MythicPlayer1@users.noreply.github.com>
2023-03-04 22:09:07 +0545

    Add files via upload

--- server/dbconnection.js
+++ server/dbconnection.js
@@ -1,8 +1,16 @@
-const mongoose= require('mongoose');
+const mongoose = require('mongoose');
 mongoose.set('strictQuery', true)
 
-const dbconnect=()=>{
-    return(mongoose.connect("mongodb://localhost:27017/E-commerce"))
+const dbconnect = async () => {
+    try {
+        await mongoose.connect("mongodb://localhost:27017/E-commerce");
+        console.log('connected')
+    }
+    catch(err) {
+            console.log('failed to conect', err)
+
+
+    }
 }
 
-module.exports=dbconnect;
+module.exports = dbconnect;
